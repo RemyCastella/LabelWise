d1fbc913350cb2e37efdcaf0c57421a6d8e920ce
aliya memah <aliamemah@gmail.com>
2025-02-25 15:08:47 +0900

    create show page

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,21 @@
 class FoodsController < ApplicationController
+
+  def show
+    @food = Food.find(params[:id])
+    @user = current_user
+    @scan = current_user.scans.where(food: @food)
+  end
+
+  def favorite
+    @food = Food.find(params[:id])
+    current_user.favorite(@food) unless current_user.favorited?(@food)
+    redirect_to food_path(@food)
+  end
+
+  def unfavorite
+    @food = Food.find(params[:id])
+    current_user.unfavorite(@food)
+    redirect_to food_path(@food)
+  end
+
 end
