f8aa20ea688a852a6556fd22137cdaeb163a4a5a
Remy Castella <remy.jpen@gmail.com>
2025-02-25 09:05:51 +0900

    add user has many foods through scans relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :scans, dependent: :destroy
   has_many :portions, dependent: :destroy
+  has_many :foods, through: :scans
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
