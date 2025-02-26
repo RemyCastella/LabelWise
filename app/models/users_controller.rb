0c6ea7f7474e1659c0e285a3e961b5bd7dd3ffdb
Prachi Gandhi <pg20022712@gmail.com>
2025-02-26 15:37:33 +0900

    profile page

---
+++ app/models/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def profile
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "Profile updated successfully!"
+      redirect_to profile_path
+    else
+      flash[:alert] = "Error updating profile. Please try again."
+      render :profile, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar, :password, :password_confirmation,
+      :calories, :protein, :carbohydrates, :fat, :sodium,
+      :vegetarian, :vegan, :keto, :pork, :beef, :gluten, :lactose, :other_ingredients)
+  end
+end
