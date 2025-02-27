57701c5bb178d556b39c4193e7ca981f1b63917d
arnold dijkhuizen <arnolddijkhuizen@hotmail.com>
2025-02-27 03:47:35 +0100

    added the daily reset

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,8 @@ class PagesController < ApplicationController
     @total_nutrition = { protein: 0, fat: 0, carbohydrates: 0 }
     @total_cal = 0
     @portions.each do |portion|
+      next unless portion.created_at.today?
+
       @food = Food.find(portion.food_id)
       @total_nutrition[:protein] += @food.protein * portion.portion_size
       @total_nutrition[:fat] += @food.fat * portion.portion_size
