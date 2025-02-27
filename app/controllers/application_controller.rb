377faa8e51403f189374c0a9370d1efeea756871
arnold dijkhuizen <arnolddijkhuizen@hotmail.com>
2025-02-27 04:30:03 +0100

    added a basic onboarding page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,45 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+        :first_name,
+        :last_name,
+        :gender,
+        :age,
+        :vegetarian,
+        :vegan,
+        :keto,
+        :high_protein,
+        :pork,
+        :beef,
+        :gluten,
+        :lactose,
+        :low_sodium,
+        :low_fat,
+        :low_carbs
+      ])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :first_name,
+      :last_name,
+      :gender,
+      :age,
+      :vegetarian,
+      :vegan,
+      :keto,
+      :high_protein,
+      :pork,
+      :beef,
+      :gluten,
+      :lactose,
+      :low_sodium,
+      :low_fat,
+      :low_carbs
+      ])
+  end
 end
