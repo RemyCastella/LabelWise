8c63bb1f46e797b116669b6885a223ff0fcd86b5
Remy Castella <remy.jpen@gmail.com>
2025-04-18 15:52:30 +0900

    implement shuffling h1 @ hero

---
+++ app/javascript/controllers/shuffle_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="shuffle"
+export default class extends Controller {
+  connect() {
+    console.log(this.element);
+    this.words = ["quickly.", "in any language.", "wherever you are."]
+    this.interval = 3000
+    this.index = 0
+    this.element.textContent = this.words[this.index]
+    this.timer = setInterval(() => {
+      this.shuffle()
+    }, this.interval)
+  }
+
+  disconnect() {
+    clearInterval(this.timer)
+  }
+
+  shuffle() {
+    this.index >= this.words.length - 1 ? this.index = 0 : this.index++
+    this.element.textContent = this.words[this.index]
+  }
+}
